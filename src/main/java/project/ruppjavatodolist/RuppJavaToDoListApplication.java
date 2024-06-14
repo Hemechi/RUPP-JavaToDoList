a2f1766d78097edffad12b6858f64b196b749d83
Hemechi <helen.leang15@gmail.com>
2024-06-14 23:47:02 +0700

    first commit

---
+++ src/main/java/project/ruppjavatodolist/RuppJavaToDoListApplication.java
@@ -0,0 +1,13 @@
+package project.ruppjavatodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuppJavaToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuppJavaToDoListApplication.class, args);
+    }
+
+}
